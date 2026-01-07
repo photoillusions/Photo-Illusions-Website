5a53b1ad3adea45ec2badd2df664b65c1b10bce3
photoillusions <photoillusions@photoillusions.us>
2026-01-07 06:44:35 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Add this line
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss() // Add this line
+    tailwindcss()
   ],
-  base: '/Photo-Illusions-Website/', 
+  base: '/Photo-Illusions-Website/',
 })
