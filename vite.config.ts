62c4c07ca7474a8bbd90511c4f690d40b63b1d6d
photoillusions <photoillusions@photoillusions.us>
2026-01-07 12:32:54 -0500

    Ready to Update
    add these files and folders

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Add this line
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss() // Add this line
+    tailwindcss()
   ],
-  base: '/Photo-Illusions-Website/', 
-})
+  base: '/',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
+});
