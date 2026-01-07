6b5c5d3771982ea483aac6912b6cdee3bfa8dc35
photoillusions <photoillusions@photoillusions.us>
2026-01-07 14:23:03 -0500

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -5,4 +5,5 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Add this line temporarily to test in your browser console
 (window as any).supabase = supabase;
